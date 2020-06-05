0eab91d458265e2b8342f243507b52fbe31df8dc
REAMED <s1071465@gm.pu.edu.tw>
2020-06-05 14:35:00 +0800

    2020上機考版本2(許禾諭)

--- app/src/main/java/tw/pu/edu/tw/s1071465/trafficlight/MainActivity.java
+++ app/src/main/java/tw/pu/edu/tw/s1071465/trafficlight/MainActivity.java
@@ -5,13 +5,15 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //設定全螢幕顯示
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -21,17 +23,33 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
-        //設定螢幕為橫式
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         setContentView(R.layout.activity_main);
     }
-
     public void StartGame(View v){
         Intent it = new Intent();
         it.setClass(this, GameActivity.class);
-        startActivity(it);
-        finish();
+        EditText GRE = (EditText) findViewById(R.id.GRE);
+        it.putExtra("GRESEC", GRE.getText().toString());
+        EditText YEL = (EditText) findViewById(R.id.YEL);
+        it.putExtra("YELSEC", YEL.getText().toString());
+        EditText RED = (EditText) findViewById(R.id.RED);
+        it.putExtra("REDSEC", RED.getText().toString());
+        if(GRE.getText().toString().matches("0")||YEL.getText().toString().matches("0")||RED.getText().toString().matches("0"))
+        {
+            Toast.makeText(this, "燈號的秒數不能為0",Toast.LENGTH_LONG).show();
+
+        }
+        else if(GRE.getText().toString().matches("")||YEL.getText().toString().matches("")||RED.getText().toString().matches("0"))
+        {
+            Toast.makeText(this, "燈號的秒數不能為空白",Toast.LENGTH_LONG)
+                    .show();
+        }
+        else {
+            startActivity(it);
+            finish();
+        }
     }
 
     public void EndApp(View v){
