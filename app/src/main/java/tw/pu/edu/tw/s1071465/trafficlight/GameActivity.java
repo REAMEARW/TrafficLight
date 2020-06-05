3577bd55a8e0e9f2a81048e2d3c3c727b4093b16
REAMED <s1071465@gm.pu.edu.tw>
2020-06-05 14:43:17 +0800

    2020上機考版本2(許禾諭)

--- app/src/main/java/tw/pu/edu/tw/s1071465/trafficlight/GameActivity.java
+++ app/src/main/java/tw/pu/edu/tw/s1071465/trafficlight/GameActivity.java
@@ -1,6 +1,8 @@
 package tw.pu.edu.tw.s1071465.trafficlight;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Canvas;
 import android.os.Bundle;
@@ -16,7 +18,13 @@ public class GameActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Intent it = getIntent();
+        String GRESEC = it.getStringExtra("GRESEC");
+        String YELSEC = it.getStringExtra("YELSEC");
+        String REDSEC = it.getStringExtra("REDSEC");
+        int gre=Integer.valueOf(GRESEC);
+        int yel=Integer.valueOf(YELSEC);
+        int red=Integer.valueOf(REDSEC);
         //設定全螢幕顯示
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -35,7 +43,7 @@ public class GameActivity extends AppCompatActivity{
         //GameSV.SetLightSec(3,2,5); //初始各燈號秒數(測試用)
 
         //設定初始測試之燈號秒數
-        GameSV.SetLightSec(6,2,3);
+        GameSV.SetLightSec(gre,yel,red);
 
         handler= new Handler();
     }
